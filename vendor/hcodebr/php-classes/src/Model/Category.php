69c7a6b0b8449fc9507ca9fa558fb14a2f90e92d
rdvasc <rdvascon@gmail.com>
2020-05-28 08:38:09 -0300

    adicao das categorias

---
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -0,0 +1,74 @@
+<?php 
+
+namespace Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+
+class Category extends Model {
+
+
+	public static function listAll()
+	{
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_categories ORDER BY descategory");
+
+	}
+
+	public function save()
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_CATEGORIES_save(:idcategory, :descategory)", array(
+			":idcategory"=>$this->getidcategory(),
+			":descategory"=>$this->getdescategory()
+		));
+
+		$this->setData($results[0]);
+
+	}
+
+	public function get($idcategory) 
+	{
+		$sql = new Sql();
+		$results=$sql->select("SELECT * FROM tb_categories WHERE idcategory = :idcategory", [
+			':idcategory'=>$idcategory
+		]);
+
+		$this->setData($results[0]);
+
+	}
+
+
+	public function delete()
+	{
+		$sql = new Sql();
+		$sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory", [
+			":idcategory"=>$this->getidcategory()
+		]);
+	}
+
+
+	public function update()
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usersupdate_save(:idcategory, :descategory, :dtregister)", array(
+			":idcategory"=>$this->getidcategory(),
+			":descategory"=>utf8_decode($this->getdescategory()),
+			":dtregister"=>$this->getdtregister()
+		));
+
+		$this->setData($results[0]);		
+
+	}
+
+
+
+}
+
+?>
